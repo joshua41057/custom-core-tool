55c69ca9576f5587810e7a8286468ed94314a17d
Joshua Yoo <yoojj4@uci.edu>
2025-05-31 09:07:40 -0700

    Update constraints and example results

--- constraints/auto_pblock.tcl
+++ constraints/auto_pblock.tcl
@@ -1,7 +1,9 @@
 # auto_pblock.tcl (generated)
-resize_pblock [get_pblocks pblock_1] -add {SLICE_X50Y840:SLICE_X54Y899}
-resize_pblock [get_pblocks pblock_1] -add {DSP48E2_X7Y336:DSP48E2_X7Y359}
+if {![llength [get_pblocks pblock_1]]} { create_pblock pblock_1 }
+resize_pblock [get_pblocks pblock_1] -add {SLICE_X31Y780:SLICE_X54Y899}
+resize_pblock [get_pblocks pblock_1] -add {DSP48E2_X7Y312:DSP48E2_X7Y359}
 
-set inst [get_cells -hier {glen[0].blk_i}]
+set inst [get_cells -hier -filter {NAME =~ "glen[*].blk_i"}]
 foreach pb [get_pblocks -of_objects $inst] { remove_cells_from_pblock $pb $inst }
 add_cells_to_pblock [get_pblocks pblock_1] $inst -clear_locs
+set_property CONTAIN_ROUTING true [get_pblocks pblock_1]
--- constraints/speeds.tcl
+++ constraints/speeds.tcl
@@ -1,21 +1,23 @@
 # speeds.tcl  - report WNS / Fmax for every generated Pblock
-set period_ps 3333      ;# ⇒ 300 MHz
-set n_case   1         ;# update if N_CASE changes
+set period_ps 3333      ;# ⇒ 300 MHz 
 
-set pb_list {}
-for {set i 1} {$i <= $n_case} {incr i} {
-    lappend pb_list [format "pblock_%d" $i]
+set pb_list [get_pblocks pblock_*]
+if {[llength $pb_list] == 0} {
+    puts "No pblocks found → nothing to report"
+    return
 }
 
 foreach pb $pb_list {
-    set cells_in_pb [get_cells -hierarchical -filter "PBLOCK == $pb"]
-    set timing_paths [get_timing_paths -from $cells_in_pb -to $cells_in_pb -max_paths 1]
-    if {[llength $timing_paths]} {
-        set wns  [get_property SLACK $timing_paths]
+    set cells   [get_cells -hier -filter "PBLOCK == $pb"]
+    set tpaths  [get_timing_paths -from $cells -to $cells -max_paths 1]
+    if {[llength $tpaths]} {
+        set wns  [get_property SLACK $tpaths]
         set fmax [expr {1000.0 / ($period_ps/1000.0 - $wns)}]
-        puts "$pb  WNS=$wns ns   Fmax=[format %.1f $fmax] MHz"
+        puts "$pb  WNS=${wns} ns   Fmax=[format %.1f $fmax] MHz"
     } else {
         puts "$pb  No timing paths found."
     }
-    report_utilization -pblocks $pb -file [format "utilization_%s.rpt" $pb]
+
+    report_utilization -quiet -pblocks $pb \
+        -file [format "utilization_%s.rpt" $pb]
 }
